a9ef11f2ca8a93719477e4178b5bbc84eed1fbf2
yaroslavrumak <people1206@ukr.net>
2017-06-18 13:24:53 +0300

    coomit lesson over

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-$('.logo').click( function() {
-  $('.nav__button').css("display", "block");
-  $('a').fadeIn();
+var i = 0;
+
+
+//click це клік 
+$('.logo').click(function() {
+//  if - якщо (умова). == дорівнює, != не дорівнює
+  if (i == 0) {
+    $('.nav__button').css('margin', '0 auto').fadeIn();
+    $('a').css('display', 'block').fadeIn();
+    i = 1;
+  }
+  else {
+    $('.nav__button').fadeOut();
+    $('a').fadeOut();
+    i = 0;    
+  }
 })
