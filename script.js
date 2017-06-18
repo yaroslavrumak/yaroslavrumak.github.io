171d50405d468c75ccea223b706ffdbfe718b9c6
yaroslavrumak <people1206@ukr.net>
2017-06-18 13:27:46 +0300

    commit lesson1

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ var i = 0;
 //click це клік 
 $('.logo').click(function() {
 //  if - якщо (умова). == дорівнює, != не дорівнює
-  if (i == 0) {
-    $('.nav__button').css('margin', '0 auto').fadeIn();
-    $('a').css('display', 'block').fadeIn();
-    i = 1;
-  }
-  else {
-    $('.nav__button').fadeOut();
-    $('a').fadeOut();
-    i = 0;    
+  if ($(window).width() < 1200) {
+    if (i == 0) {
+      $('.nav__button').css('margin', '0 auto').fadeIn();
+      $('a').css('display', 'block').fadeIn();
+      i = 1;
+    }
+    else {
+      $('.nav__button').fadeOut();
+      $('a').fadeOut();
+      i = 0;    
+    }
   }
 })
