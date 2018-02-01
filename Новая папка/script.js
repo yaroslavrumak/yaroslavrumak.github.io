f131cd46a696ced1a47b38e17dfa222f7c72fb0d
yaroslavrumak <people1206@ukr.net>
2018-02-01 21:50:27 +0200

    new changes
    1

---
+++ Новая папка/script.js
@@ -0,0 +1,19 @@
+var i = 0;
+
+
+//click це клік 
+$('.logo').click(function() {
+//  if - якщо (умова). == дорівнює, != не дорівнює
+  if ($(window).width() < 1200) {
+    if (i == 0) {
+      $('.nav__button').css('margin', '0 auto').fadeIn();
+      $('a').css('display', 'block').fadeIn();
+      i = 1;
+    }
+    else {
+      $('.nav__button').fadeOut();
+      $('a').fadeOut();
+      i = 0;    
+    }
+  }
+})
