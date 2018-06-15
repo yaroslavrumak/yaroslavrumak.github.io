f9723837e55445951832d032326357c5f5eaa723
yaroslavrumak <people1206@ukr.net>
2018-06-15 20:28:20 +0300

    9

--- box 3d/js/script.js
+++ box 3d/js/script.js
@@ -35,7 +35,7 @@ window.onload = function() {
   scene.add(light);
   
   var texture = new THREE.TextureLoader().load('img/crate.gif');
-  var geometry = new THREE.BoxGeometry(256, 256, 256);
+  var geometry = new THREE.BoxBufferGeometry(256, 256, 256);
  
   
   
