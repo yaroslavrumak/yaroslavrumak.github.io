ce2039547e8765d1136a74069699a7aac7334dc6
yaroslavrumak <people1206@ukr.net>
2018-06-16 18:59:17 +0300

    3

--- box 3d/js/script.js
+++ box 3d/js/script.js
@@ -5,11 +5,13 @@ window.onload = function () {
   var height = window.innerHeight;
   var canvas = document.getElementById('canvas');
   var targetRotation = 0;
-			var targetRotationOnMouseDown = 0;
-			var mouseX = 0;
-			var mouseXOnMouseDown = 0;
-			var windowHalfX = window.innerWidth / 2;
-			var windowHalfY = window.innerHeight / 2;
+  var targetRotationOnMouseDown = 0;
+  var mouseX = 0;
+  var mouseY = 0;
+  var mouseXOnMouseDown = 0;
+  var mouseYOnMouseDown = 0;
+  var windowHalfX = window.innerWidth / 2;
+  var windowHalfY = window.innerHeight / 2;
 
 
 
@@ -87,10 +89,24 @@ window.onload = function () {
     mouseXOnMouseDown = event.clientX - windowHalfX;
     targetRotationOnMouseDown = targetRotation;
   }
+  //
+  function onDocumentMouseDown(event) {
+    event.preventDefault();
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+    document.addEventListener('mouseup', onDocumentMouseUp, false);
+    document.addEventListener('mouseout', onDocumentMouseOut, false);
+    mouseXOnMouseDown = event.clientY - windowHalfY;
+    targetRotationOnMouseDown = targetRotation;
+  }
 
   function onDocumentMouseMove(event) {
     mouseX = event.clientX - windowHalfX;
     targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.02;
+  }
+
+  function onDocumentMouseMove(event) {
+    mouseX = event.clientY - windowHalfY;
+    targetRotation = targetRotationOnMouseDown + (mouseY - mouseYOnMouseDown) * 0.02;
   }
 
   function onDocumentMouseUp(event) {
@@ -112,12 +128,26 @@ window.onload = function () {
       targetRotationOnMouseDown = targetRotation;
     }
   }
+  function onDocumentTouchStart(event) {
+    if (event.touches.length === 1) {
+      event.preventDefault();
+      mouseXOnMouseDown = event.touches[0].pageY - windowHalfY;
+      targetRotationOnMouseDown = targetRotation;
+    }
+  }
 
   function onDocumentTouchMove(event) {
     if (event.touches.length === 1) {
       event.preventDefault();
       mouseX = event.touches[0].pageX - windowHalfX;
       targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.05;
+    }
+  }
+  function onDocumentTouchMove(event) {
+    if (event.touches.length === 1) {
+      event.preventDefault();
+      mouseX = event.touches[0].pageY - windowHalfY;
+      targetRotation = targetRotationOnMouseDown + (mouseY - mouseYOnMouseDown) * 0.05;
     }
   }
 
@@ -133,6 +163,7 @@ window.onload = function () {
     //    controls.update();
 
     scene.rotation.y = mesh.rotation.y += ( targetRotation - mesh.rotation.y ) * 0.05;
+    scene.rotation.x = mesh.rotation.x += ( targetRotation - mesh.rotation.x ) * 0.05;
     renderer.render(scene, camera);
     requestAnimationFrame(function () {
       loop();
