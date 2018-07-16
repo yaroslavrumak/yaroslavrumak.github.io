555d42fd69caaec1923c1df67a5957393ad20e22
yaroslavrumak <people1206@ukr.net>
2018-07-17 00:09:04 +0300

    gameboy

--- gameboy/js/main.js
+++ gameboy/js/main.js
@@ -1,85 +1,120 @@
-if (!Detector.webgl) Detector.addGetWebGLMessage();
-var container, stats, controls;
-var camera, scene, renderer, light;
+var windowHalfX = $(window).width();
+var windowHalfY = $(window).height();
 var video, videoImage, videoImageContext, videoTexture;
 
-var width = window.innerWidth;
-var height = window.innerHeight;
-var targetRotation = 0;
-var targetRotationOnMouseDown = 0;
-var mouseX = 0;
-var mouseXOnMouseDown = 0;
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
 
-init();
-animate();
+var targetRotation = 0,
+  targetRotationOnMouseDown = 0,
+  mouseXOnMouseDown = 0;
 
+var dis = [];
 
 function init() {
-  container = document.createElement('div');
-  document.body.appendChild(container);
 
-  if (Detector.webgl)
-    renderer = new THREE.WebGLRenderer({
-      antialias: true
-    });
-  else
-    renderer = new THREE.CanvasRenderer();
+  // scene
+ 
+  var scene = new THREE.Scene();
+  var scene2 = new THREE.Scene();
+  scene.background = new THREE.Color(0xffffff);
+  scene.add(new THREE.AmbientLight(0x999999));
 
+  // camera
 
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-  camera.position.set(0, 80, 100);
+  var camera = new THREE.PerspectiveCamera(45, $(window).width() / $(window).height(), 0.1, 1000);
 
+  scene.add(camera);
 
-  THREEx.WindowResize(renderer, camera);
-  THREEx.FullScreen.bindKey({
-    charCode: 'm'.charCodeAt(0)
+  // renderer
+
+  var renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialiasing: true
   });
+  renderer.setSize($(window).width() * 2, $(window).height() * 2)
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.shadowMapSoft = true;
+  renderer.gammaInput = true;
+  renderer.gammaOutput = true;
 
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xeeeeee);
+  // Elements
+  
 
 
-  light = new THREE.HemisphereLight(0xffffff, 0x444444);
-  light.position.set(0, 0, 50);
-//  light.target.position.set(0, 0, 0);
-  scene.add(light);
-  light = new THREE.DirectionalLight(0xffffff);
-  light.position.set(0, 0, 50);
-  light.target.position.set(0, 0, 0);
-  scene.add(light);
-  light = new THREE.DirectionalLight(0xffffff);
-  light.position.set(-100, -30, 50);
-  light.target.position.set(0, 0, 0);
-  scene.add(light);
-  light = new THREE.DirectionalLight(0xffffff);
-  light.position.set(100, -30, 50);
-  light.target.position.set(0, 0, 0);
-  scene.add(light);
-  light = new THREE.DirectionalLight(0xffffff);
-  light.position.set(-150, 50, 150);
-  light.target.position.set(0, 0, 0);
-  scene.add(light);
-  light = new THREE.DirectionalLight(0xffffff);
-  light.position.set(150, 50, 150);
-  light.target.position.set(0, 0, 0);
-  scene.add(light);
+  // LIGHTS
 
-  // model
-  new THREE.MTLLoader()
-    .setPath('img/')
-    .load('Device_gameboy.mtl', function (materials) {
-      materials.preload();
-      new THREE.OBJLoader()
-        .setMaterials(materials)
-        .setPath('img/')
-        .load('Device_gameboy.obj', function (object) {
-          object.position.set(0, 0, 0);
-          object.rotation.set(-Math.PI/18, 0, 0);
-          scene.add(object);
-        });
+  hemiLight = new THREE.HemisphereLight(0xffffff, 0x333333, 1);
+  hemiLight.color.setHSL(0, 0, 0);
+  hemiLight.groundColor.setHSL(1, 1, 1);
+  hemiLight.position.set(80, 150, 0);
+  scene.add(hemiLight);
+
+  hemiLightHelper = new THREE.HemisphereLightHelper(hemiLight, 1);
+  scene.add(hemiLightHelper);
+
+  //
+
+  dirLight = new THREE.DirectionalLight(0xffffff, .75);
+  dirLight.color.setHSL(1, 1, 1);
+  dirLight.position.set(80, 10, 0);
+  dirLight.position.multiplyScalar(30);
+  scene2.add(dirLight);
+
+  dirLight = new THREE.DirectionalLight(0xffffff, .1);
+  dirLight.color.setHSL(1, 1, 1);
+  dirLight.position.set(0, 20, 30);
+  dirLight.position.multiplyScalar(30);
+  scene2.add(dirLight);
+
+  dirLight = new THREE.DirectionalLight(0xffffff, .1);
+  dirLight.color.setHSL(1, 1, 1);
+  dirLight.position.set(0, 20, -30);
+  dirLight.position.multiplyScalar(30);
+  scene2.add(dirLight);
+
+  dirLight.castShadow = true;
+
+  dirLight.shadow.mapSize.width = 2048;
+  dirLight.shadow.mapSize.height = 2048;
+
+  var d = 10;
+
+  dirLight.shadow.camera.left = -d;
+  dirLight.shadow.camera.right = d;
+  dirLight.shadow.camera.top = d;
+  dirLight.shadow.camera.bottom = -d;
+  //
+  dirLight.shadow.camera.far = 3500;
+  dirLight.shadow.bias = -0.00001;
+
+
+
+  // MODEL
+
+  var mtlLoader = new THREE.MTLLoader();
+  mtlLoader.setPath('img/')
+  mtlLoader.load('Device_gameboy.mtl', function (materials) {
+    materials.preload();
+    for (key in materials.materials) {
+      if (materials.materials[key].map != null) {
+        materials.materials[key].color.r = 0.75;
+        materials.materials[key].color.g = 0.75;
+        materials.materials[key].color.b = 0.75;
+      }
+    }
+    materials.materials.magFilter = THREE.NearestFilter;
+    materials.materials.minFilter = THREE.LinearFilter;
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.setPath('img/')
+    objLoader.load('Device_gameboy.obj', function (object) {
+      object.position.y = 0;
+      object.position.z = 0;
+      object.rotation.set(0, Math.PI / 2, 0);
+      scene2.add(object);
     });
+  })
+  scene.add(scene2);
 
 
   // video
@@ -108,91 +143,91 @@ function init() {
     overdraw: true
   });
 
-  var movieGeometry = new THREE.PlaneGeometry(18.5, 17, 4, 4);
+  var movieGeometry = new THREE.PlaneGeometry(18.6, 16, 4, 4);
   var movieScreen = new THREE.Mesh(movieGeometry, movieMaterial);
-  movieScreen.position.set(0, 16, 4);
-  movieScreen.rotation.set(-Math.PI/18, 0, 0);
-  scene.add(movieScreen);
+  movieScreen.position.set(6.5, 15.2, 0.2);
+  movieScreen.rotation.set(0, Math.PI / 2, 0);
+  scene2.add(movieScreen);
+  scene.add(scene2);
 
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  container.appendChild(renderer.domElement);
-  window.addEventListener('resize', onWindowResize, false);
   document.addEventListener('mousedown', onDocumentMouseDown, false);
   document.addEventListener('touchstart', onDocumentTouchStart, false);
   document.addEventListener('touchmove', onDocumentTouchMove, false);
-}
+  window.addEventListener('resize', onWindowResize, false);
 
-//
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
+  $('#block').append(renderer.domElement)
 
-function onDocumentMouseDown(event) {
-  event.preventDefault();
-  document.addEventListener('mousemove', onDocumentMouseMove, false);
-  document.addEventListener('mouseup', onDocumentMouseUp, false);
-  document.addEventListener('mouseout', onDocumentMouseOut, false);
-  mouseXOnMouseDown = event.clientX - windowHalfX;
-  targetRotationOnMouseDown = targetRotation;
-}
+  function onWindowResize() {
+    windowHalfX = $(window).width() / 2;
+    windowHalfY = $(window).height() / 2;
 
-function onDocumentMouseMove(event) {
-  mouseX = event.clientX - windowHalfX;
-  targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.02;
-}
+    camera.aspect = $(window).width() / $(window).height();
+    camera.updateProjectionMatrix();
 
-function onDocumentMouseUp(event) {
-  document.removeEventListener('mousemove', onDocumentMouseMove, false);
-  document.removeEventListener('mouseup', onDocumentMouseUp, false);
-  document.removeEventListener('mouseout', onDocumentMouseOut, false);
-}
+    renderer.setSize($(window).width() * 2, $(window).height() * 2);
+    renderer.render(scene, camera);
+  }
 
-function onDocumentMouseOut(event) {
-  document.removeEventListener('mousemove', onDocumentMouseMove, false);
-  document.removeEventListener('mouseup', onDocumentMouseUp, false);
-  document.removeEventListener('mouseout', onDocumentMouseOut, false);
-}
-
-function onDocumentTouchStart(event) {
-  if (event.touches.length === 1) {
-    event.preventDefault();
-    mouseXOnMouseDown = event.touches[0].pageX - windowHalfX;
+  function onDocumentMouseDown(e) {
+    e.preventDefault();
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+    document.addEventListener('mouseup', onDocumentMouseUp, false);
+    document.addEventListener('mouseout', onDocumentMouseOut, false);
+    mouseXOnMouseDown = e.clientX - windowHalfX;
     targetRotationOnMouseDown = targetRotation;
   }
-}
 
-function onDocumentTouchMove(event) {
-  if (event.touches.length === 1) {
-    event.preventDefault();
-    mouseX = event.touches[0].pageX - windowHalfX;
-    targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.05;
+  function onDocumentMouseMove(e) {
+    mouseX = e.clientX - windowHalfX;
+    targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.02;
   }
-}
 
+  function onDocumentMouseUp(e) {
+    document.removeEventListener('mousemove', onDocumentMouseMove, false);
+    document.removeEventListener('mouseup', onDocumentMouseUp, false);
+    document.removeEventListener('mouseout', onDocumentMouseOut, false);
+  }
 
-//
-function animate() {
-  requestAnimationFrame(animate);
-  scene.rotation.y = scene.rotation.y += (targetRotation - scene.rotation.y) * 0.05;
+  function onDocumentMouseOut(e) {
+    document.removeEventListener('mousemove', onDocumentMouseMove, false);
+    document.removeEventListener('mouseup', onDocumentMouseUp, false);
+    document.removeEventListener('mouseout', onDocumentMouseOut, false);
+  }
+
+  function onDocumentTouchStart(e) {
+    if (e.touches.length === 1) {
+      e.preventDefault();
+      mouseXOnMouseDown = e.touches[0].pageX - windowHalfX;
+      targetRotationOnMouseDown = targetRotation;
+    }
+  }
+
+  function onDocumentTouchMove(e) {
+    if (e.touches.length === 1) {
+      e.preventDefault();
+      mouseX = e.touches[0].pageX - windowHalfX;
+      targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.05;
+    }
+  }
+
   render();
-}
 
-function render() {
-  if (video.readyState === video.HAVE_ENOUGH_DATA) {
-    videoImageContext.drawImage(video, 0, 0);
-    if (videoTexture)
-      videoTexture.needsUpdate = true;
+  function render() {
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+      videoImageContext.drawImage(video, 0, 0);
+      if (videoTexture)
+        videoTexture.needsUpdate = true;
+    }
+    
+    scene2.rotation.y = scene2.rotation.y += (targetRotation - scene2.rotation.y) * 0.05;
+    var timer = Date.now() * 0.0001;
+    camera.position.x = Math.cos(timer) * 130;
+    camera.position.y = 50;
+    camera.position.z = Math.sin(timer) * 130;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
   }
-
-  var timer = Date.now() * 0.0001;
-  camera.position.x = Math.cos(timer) * 130;
-  camera.position.y = 50;
-  camera.position.z = Math.sin(timer) * 130;
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
-  renderer.render(scene, camera);
 }
+
+window.onload = init;
